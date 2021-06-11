31b028c26bb171c412f0d531390aa8bb0369c2c2
root <root@DESKTOP-HDLMPH8.localdomain>
2021-06-11 11:25:25 +0800

    2021.06.11 sc.lin Chg: Finished executing algorithm bash, but it is very rough.

--- MockLinuxStories/ExecuteAlgorithm.sh
+++ MockLinuxStories/ExecuteAlgorithm.sh
@@ -1,3 +1,21 @@
-App=$1
-FolderName=$2
-echo "Execute application $App"
+#!/bin/bash
+FolderName=$1
+
+mkdir $FolderName
+cd $FolderName
+#for i in {1..10}; 
+#do 
+#	#../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test$i.txt
+#	echo $i
+#done
+
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test1.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test2.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test3.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test4.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test5.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test6.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test7.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test8.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test9.txt
+../MockLinuxStories/MockAlgorithm/build/MockAlgorithm > test10.txt
